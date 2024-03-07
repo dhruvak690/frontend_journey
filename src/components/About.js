ea514a7b2b330e352ae0df877b673d94eda395ff
dhruvak690 <dhruvak690@gmail.com>
2024-03-07 13:15:39 +0530

    Added multiple pages to our web application and used react router to access these pages

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () =>{
+    return(
+        <div>
+            <h1>About us</h1>
+            <h2>This is my front end learning and implementing journey</h2>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () =>{
+    return(
+        <div>
+            <h2>contact info: dhruvk678@gmail.com</h2>
+            <h2>Sector 17A gurugram</h2>
+        </div>
+    );
+};
+
+export default Contact;
